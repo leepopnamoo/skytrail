21d0e31234d1dab44ca68dced5612d9c19043f28
leepopnamoo <fingertree@gmail.com>
2024-10-26 15:01:47 +0900

    edit pdf create

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1255118290"
+    serviceWorkerVersion: "2042215348"
   }
 });
